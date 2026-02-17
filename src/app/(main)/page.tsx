e1a3f0b3dd4dc650bf3ecc2b955ebfb3fd6cee6d
HighRol1er <hunterjoe9999@gmail.com>
2026-02-18 04:36:57 +0900

    fix: Changed to <div> tag

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -2,10 +2,10 @@ import { PostList } from '@/components/post-card/PostList';
 
 export default function Main() {
   return (
-    <main className='max-w-7xl mx-auto'>
+    <div className='max-w-7xl mx-auto'>
       <PostList />
       <PostList />
       <PostList />
-    </main>
+    </div>
   );
 }
