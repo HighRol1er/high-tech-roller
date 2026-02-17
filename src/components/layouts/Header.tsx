0194146d7fbdd860fe867550a9862d2d77402f3c
HighRol1er <hunterjoe9999@gmail.com>
2026-02-18 04:33:10 +0900

    refactor: Tilte 대소문자 분리

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -4,13 +4,14 @@ import { usePathname } from 'next/navigation';
 import { SidebarTrigger } from '@/components/ui/sidebar';
 
 export function Header() {
-  const pathname = usePathname();
+  const pathname = usePathname().slice(1);
+  const formatPath = pathname.charAt(0).toUpperCase() + pathname.slice(1).toLowerCase();
 
   return (
     <header className='flex w-full items-center h-16 pl-2 sticky top-0 bg-background/50 backdrop-blur-sm z-10'>
       <SidebarTrigger />
       {/* TODO: 해당 pathname 개별 title 컴포넌트로 빼버리기  */}
-      <h1 className='text-lg font-semibold'>{pathname === '/' ? 'main' : pathname}</h1>
+      <h1 className='text-lg font-semibold'>{pathname === '' ? 'Home' : formatPath}</h1>
     </header>
   );
 }
