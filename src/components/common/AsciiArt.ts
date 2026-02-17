ad4f1b015d23b2aa7f9ddcb37ae293b76dd45c14
HighRol1er <hunterjoe9999@gmail.com>
2026-02-18 02:00:21 +0900

    chore: Ascii art 파일 이동

--- src/components/common/AsciiArt.ts
+++ src/components/common/AsciiArt.ts
@@ -2,7 +2,7 @@
 
 import { useEffect } from 'react';
 
-export default function AsciiArt() {
+export function AsciiArt() {
   useEffect(() => {
     const lines = [
       ' ██╗  ██╗██╗ ██████╗ ██╗  ██╗████████╗███████╗ ██████╗██╗  ██╗',
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,2 +1,3 @@
 export * from './Logo';
 export * from './DarkMode';
+export * from './AsciiArt';
