5ac4d59a57191a934b3a901f6bedfec774ff9c08
HighRol1er <hunterjoe9999@gmail.com>
2026-02-20 06:50:11 +0900

    fix: Build error

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,13 +1,27 @@
+import { Suspense } from 'react';
 import { PostList } from '@/components/post-card';
 import { db } from '@/db';
 import { posts } from '@/db/schema/post';
 import { desc } from 'drizzle-orm';
 
+const getAllPosts = async () => {
+  try {
+    const allPosts = await db.select().from(posts).orderBy(desc(posts.createdAt));
+    return allPosts;
+  } catch (error) {
+    console.error('데이터베이스 조회 에러 (getAllPosts):', error);
+    throw new Error('포스트 목록을 불러오는 중 문제가 발생했습니다.');
+  }
+};
+
 export default async function Main() {
-  const allPosts = await db.select().from(posts).orderBy(desc(posts.createdAt));
+  const allPosts = await getAllPosts();
+
   return (
     <div className='max-w-7xl mx-auto'>
-      <PostList allPosts={allPosts} />
+      <Suspense fallback={<div>포스트 목록을 불러오는 중...</div>}>
+        <PostList allPosts={allPosts} />
+      </Suspense>
     </div>
   );
 }
