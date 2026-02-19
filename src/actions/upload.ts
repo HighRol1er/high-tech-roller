39299ba20e8391989f78b1e37a938f824f4519bf
HighRol1er <hunterjoe9999@gmail.com>
2026-02-20 07:30:46 +0900

    fix: 제발되ㄹ.ㅏ

--- src/actions/upload.ts
+++ src/actions/upload.ts
@@ -1,13 +1,14 @@
 'use server';
 
-import { createClient } from '@/lib/supabase/server'; // 서버용 클라이언트 사용
-import sharp from 'sharp';
+import { createClient } from '@/lib/supabase/server';
 
 export const uploadPostImageAction = async (formData: FormData) => {
-  // 개발 환경인지 체크 (선택 사항: 더 안전하게 관리하고 싶을 때)
   if (process.env.NODE_ENV !== 'development') {
     throw new Error('운영 환경에서는 업로드를 지원하지 않습니다.');
   }
+
+  // ✅ 런타임에서 sharp 로드
+  const sharp = (await import('sharp')).default;
 
   const file = formData.get('file') as File;
   if (!file) throw new Error('파일이 없습니다.');
@@ -17,18 +18,18 @@ export const uploadPostImageAction = async (formData: FormData) => {
 
   const optimizedBuffer = await sharp(buffer)
     .resize(1200, null, {
-      withoutEnlargement: true, // 원본보다 커지지 않게
+      withoutEnlargement: true,
       fit: 'inside',
     })
-    .webp({ quality: 75, effort: 6 }) // 품질 75, 압축 노력 최대
+    .webp({ quality: 75, effort: 6 })
     .toBuffer();
 
   const supabase = await createClient();
+
   const fileName = `${crypto.randomUUID()}.webp`;
   const filePath = `${process.env.NEXT_PUBLIC_UPLOAD_KEY}/${fileName}`;
 
-  // 3. Supabase Storage 업로드
-  const { data, error } = await supabase.storage.from('post').upload(filePath, optimizedBuffer, {
+  const { error } = await supabase.storage.from('post').upload(filePath, optimizedBuffer, {
     contentType: 'image/webp',
     upsert: false,
   });
