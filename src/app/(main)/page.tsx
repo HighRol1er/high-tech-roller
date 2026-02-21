fdfec36771d5e1ec0e90e822fa9ca0b7ce17ea38
HighRol1er <hunterjoe9999@gmail.com>
2026-02-22 08:57:27 +0900

    fix: fetch로 변경

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,23 +3,42 @@ export const revalidate = false; // 빌드 타임에만 생성
 
 import { Loader } from '@/components/common';
 import { PostList } from '@/components/post-card';
-import { supabase } from '@/lib/supabase';
+// import { supabase } from '@/lib/supabase';
 import { Suspense } from 'react';
 
+// const getAllPosts = async () => {
+//   try {
+//     const { data, error } = await supabase
+//       .from('posts')
+//       .select('id, title, tags, slug, created_at')
+//       .order('created_at', { ascending: false });
+
+//     if (error) throw error;
+
+//     return data;
+//   } catch (error) {
+//     console.error('error', error);
+//     return [];
+//   }
+// };
+
 const getAllPosts = async () => {
-  try {
-    const { data, error } = await supabase
-      .from('posts')
-      .select('id, title, tags, slug, created_at')
-      .order('created_at', { ascending: false });
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/posts?select=id,title,tags,slug,created_at&order=created_at.desc`,
+    {
+      headers: {
+        apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!}`,
+      },
+      cache: 'force-cache',
+    },
+  );
 
-    if (error) throw error;
-
-    return data;
-  } catch (error) {
-    console.error('error', error);
-    return [];
+  if (!res.ok) {
+    throw new Error('Get Post Error');
   }
+
+  return res.json();
 };
 
 export default async function Main() {
