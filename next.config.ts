eab89b5c16d85019d64d8afd64aec851a2f8d249
HighRol1er <hunterjoe9999@gmail.com>
2026-02-22 03:31:06 +0900

    fix: bundle size

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['sharp'],
+  outputFileTracingExcludes: {
+    '*': ['@vercel/og'],
+  },
 };
 
 export default nextConfig;
