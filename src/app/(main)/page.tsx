9dc3e75b139fa064093750b9522635689132fc69
HighRol1er <hunterjoe9999@gmail.com>
2026-02-22 10:01:28 +0900

    test: 페칭 로직 원상복귀

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,57 +3,57 @@ export const revalidate = false; // 빌드 타임에만 생성
 
 import { Loader } from '@/components/common';
 import { PostList } from '@/components/post-card';
-// import { supabase } from '@/lib/supabase';
+import { supabase } from '@/lib/supabase';
 import { Suspense } from 'react';
 
-// const getAllPosts = async () => {
-//   try {
-//     const { data, error } = await supabase
-//       .from('posts')
-//       .select('id, title, tags, slug, created_at')
-//       .order('created_at', { ascending: false });
-
-//     if (error) throw error;
-
-//     return data;
-//   } catch (error) {
-//     console.error('error', error);
-//     return [];
-//   }
-// };
-
 const getAllPosts = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/posts?select=id,title,tags,slug,created_at&order=created_at.desc`,
-    {
-      headers: {
-        apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!}`,
-      },
-      cache: 'force-cache',
-    },
-  );
+  try {
+    const { data, error } = await supabase
+      .from('posts')
+      .select('id, title, tags, slug, created_at')
+      .order('created_at', { ascending: false });
 
-  if (!res.ok) {
-    throw new Error('Get Post Error');
+    if (error) throw error;
+
+    return data;
+  } catch (error) {
+    console.error('error', error);
+    return [];
   }
-
-  return res.json();
 };
 
-async function PostListContainer() {
-  const allPosts = await getAllPosts();
-  return <PostList allPosts={allPosts} />;
-}
+// const getAllPosts = async () => {
+//   const res = await fetch(
+//     `${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/posts?select=id,title,tags,slug,created_at&order=created_at.desc`,
+//     {
+//       headers: {
+//         apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+//         Authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!}`,
+//       },
+//       cache: 'force-cache',
+//     },
+//   );
 
-export default function Main() {
+//   if (!res.ok) {
+//     throw new Error('Get Post Error');
+//   }
+
+//   return res.json();
+// };
+
+// async function PostListContainer() {
+//   const allPosts = await getAllPosts();
+//   return <PostList allPosts={allPosts} />;
+// }
+
+export default async function Main() {
   console.log('RENDERED AT:', new Date().toISOString());
-  // const allPosts = await getAllPosts();
+  const allPosts = await getAllPosts();
 
   return (
     <div className='max-w-7xl mx-auto'>
       <Suspense fallback={<Loader />}>
-        <PostListContainer />
+        <PostList allPosts={allPosts} />;
       </Suspense>
     </div>
   );
